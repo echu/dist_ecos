c9a3985b27aaafde55459ecf100071e2516964ff
AJ Friend <ajfriend@gmail.com>
2013-11-20 18:55:44 -0800

    removed rformat folder

--- dist_ecos/rformat/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Functions for converting to and from ECOS/socp_data format to R-format,
-    which is of the form Rx <= s, where the cones related to the generalized
-    inequality are stored in some array cone_array. These cones can include
-    the zero cone to represent linear equality constraints.
-"""
